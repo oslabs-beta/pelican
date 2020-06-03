25d5bc4478af5eb45748bc68cfb85fbc1fcef6a7
Keiran Carpen <keirancarpen@gmail.com>
2020-06-03 14:05:52 -0400

    configured jest+enzyme testing suite and tested the app component

--- client/index.tsx
+++ client/index.tsx
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-class App extends Component<{}, {}> {
-  render() {
-    return <div> Hello world </div>;
-  }
-}
-
-export default App;
 ReactDOM.render(<App />, document.querySelector('#root'));
