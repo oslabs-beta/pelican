df05f9b440897137a08401b70f9e70bcc6d08f92
a-straus <straustech@gmail.com>
2020-06-06 22:23:01 -0400

    little more in Node and PodController

--- server/routes/local.js
+++ server/routes/local.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const PodController = require('../controllers/PodController');
+const NodeController = require('../controllers/NodeController');
 
 const localRouter = express.Router();
 
-localRouter.get(
-  '/pods',
-  (req, res, next) => {
-    console.log('in router');
-    next();
-  },
-  PodController.getPods,
-  (req, res, next) => {
-    res.sendStatus(200);
-  }
-);
+localRouter.get('/pods', PodController.getPods, (req, res, next) => {
+  res.status(200).json(res);
+});
+
+localRouter.get('/nodes', NodeController.getNodes, (req, res, next) => {
+  res.status(200).json(res);
+});
 
 module.exports = localRouter;
