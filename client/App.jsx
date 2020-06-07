d0f1cf1f1c0dc0ba69bd7757563a4e0fb445a4df
Keiran Carpen <keirancarpen@gmail.com>
2020-06-07 14:22:43 -0400

    got rid of an unused import

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import LoginContainer from './containers/LoginContainer';
 
 function App() {
