8ceeeacf64e4686b48ff8019e6a33f68a78c3cef
Derek Lauziere <dereklauziere@Dereks-Air.home>
2020-06-09 19:41:33 -0400

    front end work

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 // import LoginContainer from './containers/LoginContainer';
-import MockShowPods from "./containers/MockShowPods.jsx";
-import PodsContainer from "./components/PodsContainer.jsx";
-import PodsTable from "./components/PodsTable.jsx";
+import MockShowPods from './containers/MockShowPods.jsx';
+import PodsContainer from './components/PodsContainer.jsx';
+import CollapsibleTable from './components/CollapsibleTable.jsx';
+import './stylesheets/styles.scss';
 
 function App() {
   // return <LoginContainer />;
   // return <MockShowPods />;
   //return <PodsContainer />;
-  return <PodsTable />;
+  return <CollapsibleTable />;
 }
 
 export default App;
