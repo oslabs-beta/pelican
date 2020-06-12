80edc5491394b376bc6b121229da5090be30928f
a-straus <straustech@gmail.com>
2020-06-12 12:07:57 -0400

    added namespace backend route

---
+++ server/controllers/NamespaceController.js
@@ -0,0 +1,16 @@
+module.exports = {
+  getNamespaces: async (req, res, next) => {
+    try {
+      res.locals.namespaces = (
+        await res.locals.client.api.v1.namespaces.get()
+      ).body.items.map((namespace) => namespace.metadata.name);
+      next();
+    } catch (err) {
+      next({
+        log: `Encountered an error in NameSpaceController.getNamespaces: ${err}`,
+        status: 400,
+        message: 'An error occured fetching namespace',
+      });
+    }
+  },
+};
