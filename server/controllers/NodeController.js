2f72a0ef0557172084441055ff23fc4640f5f428
a-straus <straustech@gmail.com>
2020-06-12 11:16:02 -0400

    refactored controllers to set res.locals in one line

--- server/controllers/NodeController.js
+++ server/controllers/NodeController.js
@@ -3,27 +3,13 @@ const kubernetes = require('../../k8s-client/config');
 module.exports = {
   getNodes: async (req, res, next) => {
     try {
-      const response = await kubernetes.api.v1.nodes.get();
-      res.locals.nodes = response.body.items;
+      res.locals.nodes = (await kubernetes.api.v1.nodes.get()).body.items;
       next();
     } catch (err) {
       next({
         log: `Encountered an error in NodeController.getNodes: ${err}`,
         status: 400,
         message: 'An error occured fetching nodes',
-      });
-    }
-  },
-  getNode: async (req, res, next) => {
-    try {
-      const response = await kubernetes.api.v1.nodes('minikube').status.get();
-      console.log(response);
-      next();
-    } catch (err) {
-      next({
-        log: `Encountered an error in NodeController.getNode(name): ${err}`,
-        status: 400,
-        message: 'An error occured fetching node',
       });
     }
   },
--- server/controllers/ServiceController.js
+++ server/controllers/ServiceController.js
@@ -3,11 +3,12 @@ const kubernetes = require('../../k8s-client/config');
 module.exports = {
   getServices: async (req, res, next) => {
     try {
-      const response = await kubernetes.api.v1
-        .namespaces('default')
-        .services() //maybe no need
-        .get();
-      res.locals.services = response.body.items;
+      res.locals.services = (
+        await kubernetes.api.v1
+          .namespaces('default')
+          .services() //maybe no need
+          .get()
+      ).body.items;
       next();
     } catch (err) {
       next({
