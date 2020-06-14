de1744c7459e44796572a7575de532414d774ecd
Derek Lauziere <dereklauziere@Dereks-Air.home>
2020-06-13 23:43:51 -0400

    added sign in page temporarily set it to static load

--- client/LoginPage.jsx
+++ client/LoginPage.jsx
@@ -18,7 +18,7 @@ function Copyright() {
     <Typography variant='body2' color='textSecondary' align='center'>
       {'Copyright © '}
       <Link color='inherit' href='https://material-ui.com/'>
-        Your Website
+        Pelican
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- client/index.js
+++ client/index.js
@@ -4,10 +4,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App.jsx';
 import LoadingIndicator from './LoadingIndicator.jsx';
+import LoginPage from './LoginPage.jsx';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <LoginPage />
+
+    {/* <App /> */}
     <LoadingIndicator />
   </Provider>,
   document.querySelector('#root')
