2121f174075c766bb359ed3df9a8291bd226b94b
a-straus <straustech@gmail.com>
2020-06-15 16:26:33 -0400

    added Node route and Contorller methods

--- server/controllers/NodeController.js
+++ server/controllers/NodeController.js
@@ -1,13 +1,29 @@
 module.exports = {
   getNodes: async (req, res, next) => {
     try {
-      res.locals.nodes = (await res.locals.client.api.v1.nodes.get()).body.items;
+      res.locals.nodes = (
+        await res.locals.client.api.v1.nodes.get()
+      ).body.items;
       next();
     } catch (err) {
       next({
         log: `Encountered an error in NodeController.getNodes: ${err}`,
         status: 400,
         message: 'An error occured fetching nodes',
+      });
+    }
+  },
+  updateNode: async (req, res, next) => {
+    try {
+      await res.locals.client.apis.v1
+        .nodes(req.query.name)
+        .put({ body: req.body });
+      next();
+    } catch (err) {
+      next({
+        log: `Encountered an error in NodeController.update: ${err}`,
+        status: 500,
+        message: 'An error occured updating the node',
       });
     }
   },
--- server/routes/resourceRoutes/nodeRouter.js
+++ server/routes/resourceRoutes/nodeRouter.js
@@ -5,5 +5,7 @@ const nodeRouter = express.Router();
 nodeRouter.get('/', NodeController.getNodes, (req, res, next) => {
   return res.status(200).json(res.locals.nodes);
 });
-
+nodeRouter.put('/', NodeController.updateNode, (req, res, next) => {
+  return res.sendStatus(200);
+});
 module.exports = nodeRouter;
