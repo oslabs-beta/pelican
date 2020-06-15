4f720cd6b61762f573c3ac9935686b2688da9be0
Derek Lauziere <dereklauziere@Dereks-Air.home>
2020-06-15 12:06:47 -0400

    forms working, next is map function

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable import/extensions */
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import SideBar, { TopBar } from './components/Navbar.jsx';
 import PodTable from './components/pods/PodTable.jsx';
 import NodeTable from './components/nodes/NodeTable.jsx';
 import ServiceTable from './components/services/ServiceTable.jsx';
 import DeploymentTable from './components/deployments/DeploymentTable.jsx';
-import YamlConfiguration from './components/YamlConfigurations.jsx';
+import YamlConfiguration from './components/Configurations/YamlConfigurations.jsx';
 import RefreshRoute from './RefreshRoute.jsx';
 import './stylesheets/styles.scss';
 // import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
@@ -22,14 +27,26 @@ function App() {
           <Route exact path="/nodes" component={NodeTable} />
           <Route exact path="/deployments" component={DeploymentTable} />
           <Route exact path="/services" component={ServiceTable} />
-          <RefreshRoute path="/pods/:name" component={YamlConfiguration} root="pods" />
-          <RefreshRoute path="/nodes/:name" component={YamlConfiguration} root="nodes" />
+          <RefreshRoute
+            path="/pods/:name"
+            component={YamlConfiguration}
+            root="pods"
+          />
+          <RefreshRoute
+            path="/nodes/:name"
+            component={YamlConfiguration}
+            root="nodes"
+          />
           <RefreshRoute
             path="/deployments/:name"
             component={YamlConfiguration}
             root="deployments"
           />
-          <RefreshRoute path="/services/:name" component={YamlConfiguration} root="services" />
+          <RefreshRoute
+            path="/services/:name"
+            component={YamlConfiguration}
+            root="services"
+          />
           <Route path="*">
             <Redirect to="/pods" />
           </Route>
--- client/LoginPage.jsx
+++ client/LoginPage.jsx
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random?nautical,pelican)',
+    backgroundImage: 'url(https://source.unsplash.com/random?nautical)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light'
@@ -160,11 +160,11 @@ export default function SignInSide() {
             <Grid container>
               <Grid item xs>
                 <Link
-                  href="https://cloud.google.com/kubernetes-engine"
+                  href="#"
                   variant="body2"
                   style={{ textDecoration: 'none' }}
                 >
-                  Supports Google GKE
+                  Supports Local Clusters
                 </Link>
               </Grid>
               <Grid item>
