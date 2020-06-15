74ca00fab4fe8ea22bd53f0d3bd416720c441e1f
a-straus <straustech@gmail.com>
2020-06-15 12:09:34 -0400

    Fixed Dropdowns

--- client/components/services/ServiceRow.jsx
+++ client/components/services/ServiceRow.jsx
@@ -43,8 +43,7 @@ const useStyles = makeStyles({
   },
 });
 
-function Row(props) {
-  const { service } = props;
+function ServiceRow({ service }) {
   const [open, setOpen] = React.useState(false);
   const classes = useStyles();
 
@@ -66,7 +65,11 @@ function Row(props) {
     <>
       <StyledTableRow className={classes.table}>
         <StyledTableCell>
-          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </StyledTableCell>
@@ -81,34 +84,69 @@ function Row(props) {
         </StyledTableCell>
       </StyledTableRow>
       <TableRow>
-        <StyledTableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <StyledTableCell
+          style={{ paddingBottom: 0, paddingTop: 0 }}
+          colSpan={6}
+        >
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                Logs:
+              <Typography variant="subtitle1" gutterBottom component="div">
+                Match Labels on Pods:
               </Typography>
-              <Table size="small" aria-label="purchases">
+              <Table size="small" aria-label="logs">
                 <TableHead>
                   <TableRow>
-                    <TableCell>logs1</TableCell>
-                    <TableCell>logs2</TableCell>
-                    <TableCell align="right">logs3</TableCell>
-                    <TableCell align="right">more logs</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }}>Key</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }} align="left">
+                      Value
+                    </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {['stuff', 'otherstuff'].map((row, i) => (
-                    <StyledTableRow key={i}>
-                      <StyledTableCell component="th" scope="row">
-                        {row}
-                      </StyledTableCell>
-                      <StyledTableCell>{row}</StyledTableCell>
-                      <StyledTableCell align="right">{row}</StyledTableCell>
-                      <StyledTableCell align="right">
-                        {Math.round(1 * 5 * 100) / 100}
-                      </StyledTableCell>
-                    </StyledTableRow>
-                  ))}
+                  {service.spec.selector
+                    ? Object.keys(service.spec.selector).map((key, i) => (
+                        <StyledTableRow key={`deploymentCondition${i}`}>
+                          <StyledTableCell component="th" scope="row">
+                            {key}
+                          </StyledTableCell>
+                          <StyledTableCell>
+                            {service.spec.selector[key]}
+                          </StyledTableCell>
+                        </StyledTableRow>
+                      ))
+                    : null}
+                </TableBody>
+              </Table>
+              <Typography variant="subtitle1" gutterBottom component="div">
+                Ports:
+              </Typography>
+              <Table size="small" aria-label="logs">
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ fontWeight: 'bold' }}>
+                      Protocol
+                    </TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }}>Port</TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }}>
+                      Target Port
+                    </TableCell>
+                    <TableCell style={{ fontWeight: 'bold' }} align="left">
+                      Node Port
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {service.spec.ports
+                    ? service.spec.ports.map((portObj, i) => (
+                        <StyledTableRow key={`servicePortsRow${i}`}>
+                          {Object.values(portObj).map((value, i) => (
+                            <StyledTableCell key={`servicePortValueRow${i}`}>
+                              {value}
+                            </StyledTableCell>
+                          ))}
+                        </StyledTableRow>
+                      ))
+                    : null}
                 </TableBody>
               </Table>
             </Box>
@@ -120,4 +158,4 @@ function Row(props) {
   );
 }
 
-export default Row;
+export default ServiceRow;
